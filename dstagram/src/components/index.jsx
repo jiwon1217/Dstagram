16081d38c89d3d47e10446cc09e8e5b44b13614e
JIWON KWAK <koagjw@gmail.com>
2020-07-22 22:53:30 +0900

    components/index.js 생성, Header 컴포넌트에 PopupState 추가

---
+++ dstagram/src/components/index.jsx
@@ -0,0 +1,6 @@
+export {default as SignUp} from './SignUp';
+export {default as Login} from './Login';
+export {default as Header} from './Header';
+export {default as Posting} from './Posting';
+export {default as Feed} from './Feed';
+export {default as Footer} from './Footer';
